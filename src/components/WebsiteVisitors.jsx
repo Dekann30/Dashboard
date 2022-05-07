c0b115fb8882338c7164d9486ff079b99e624585
Deanna K <keysdeanna@gmail.com>
2022-05-07 06:09:48 -0400

    adds css and fake data to website visitor component

--- src/components/WebsiteVisitors.jsx
+++ src/components/WebsiteVisitors.jsx
@@ -4,6 +4,8 @@ const WebsiteVisitors = function () {
     return (
         <div className='WebVisi'>
             <h2>Website Visitor</h2>
+            <h3>8578</h3>
+            <div className='overlay'></div>
         </div>
     )
 }
